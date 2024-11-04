10a6443c13aa24c865d3836e10c51ddf4da09d27
RHrakib1 <rakibhasanmd457@gmail.com>
2024-11-04 07:39:43 +0600

    add a react router and set up daisy ui,tailween and navbar

---
+++ src/Component/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>404 - Page Not Found</h1>
+            <p>Sorry, the page you are looking for was not found।</p>
+            <a href="/">Go to home page</a>
+        </div>
+    );
+};
+
+export default ErrorPage;
