52be239aed1f4eef3512eebe89df2640d69f8943
RHrakib1 <rakibhasanmd457@gmail.com>
2024-11-05 13:49:02 +0600

    add to the link

--- src/Component/AllTourists/ViewAll.jsx
+++ src/Component/AllTourists/ViewAll.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const ViewAll = () => {
     const tour = useLoaderData();
     const { _id, spotName, location, averageCost, travleTime, countryName, shortDes, seasonality, totalvisit, url, rating } = tour
 
     return (
-        <div>
-            <h1>hi</h1>
+        <div className=''>
+            <div className=" rounded-t-xl bg-gray-700 shadow-2xl">
+                <figure>
+                    <img className='w-full rounded-t-xl'
+                        src={url} />
+                </figure>
+                <div className="p-5">
+                    <div className='flex justify-between'>
+                        <p className='font-bold text-2xl'>{countryName}</p>
+                        <p className='font-bold text-2xl'>{travleTime}</p>
+                    </div>
+                    <h2 className="mt-2 font-extrabold text-3xl card-title">{spotName}</h2>
+                    <p>{shortDes}</p>
+                    <div className="card-actions justify-end">
+                       <Link to='/alltourists'> <button  className="btn btn-primary">Go Back</button></Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
